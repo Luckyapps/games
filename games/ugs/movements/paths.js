a48d3097823cb8c8d5ead2f714d57c3d9076c6e7
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2023-03-19 21:01:19 +0100

    introducing: the first version of ugs

---
+++ games/ugs/movements/paths.js
@@ -0,0 +1,14 @@
+var movements = {
+    jump:{
+        button:"h",
+        transition: "bottom 1s",
+        path:[
+            {   posx: {position: "this",transform: 0},
+                posy: {position: "this", transform: +30}
+            },
+            {   posx: {position: "this",transform: 0},
+                posy: {position: "this", transform: 0}
+            }
+        ]
+    }
+}
---
+++ games/ugs/objects/objects.js
@@ -0,0 +1,23 @@
+var objectlist = {
+    player1:{
+        width: 50,
+        height: 50,
+        color: "red",
+        pos: false
+    },
+    player2:{
+        width: 100,
+        height: 50,
+        color: "yellow",
+        pos: {
+            x:100,
+            y:100,
+        }
+    },
+    player3:{
+        width: 300,
+        height: 20,
+        color: "orange",
+        pos: false
+    }
+}
